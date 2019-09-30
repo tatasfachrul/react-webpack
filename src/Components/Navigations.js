4c47379e88a7e3e2610d436cb708d35bb25903f1
Tatas Fachrul <tatasfachrul@gmail.com>
2019-09-30 14:42:12 +0700

    first commit

---
+++ src/Components/Navigations.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+
+class Navigations extends Component {
+  render() {
+    return(
+      <div>
+        <ul>
+          <li>
+            <Link to={'/menu'}>Menu</Link>
+          </li>
+          <li>
+            <Link to={'/profile'}>Profile</Link>
+          </li>
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default Navigations
---
+++ src/Screens/Menu.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+import ProductList from '../Components/ListProduct'
+
+class Menu extends Component {
+  constructor(props) {
+    super()
+    this.state = {
+      data: []
+    }
+  }
+
+  async componentDidMount() {
+    await this.getAll()
+    console.log('ComponentDidMount', this.state.data)
+  }
+
+  getAll = async () => {
+    await axios.get('http://localhost:3020/menu')
+      .then(result => {
+        console.log(result.data.data)
+        this.setState({data: result.data.data})
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+
+  render() {
+    return (
+      <div>
+        This is Menu!
+       <ProductList menu={this.state.data} />
+      </div>
+    )
+  }
+}
+
+export default Menu
